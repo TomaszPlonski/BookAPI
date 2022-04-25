814814fc10b0d1aa3857707806d22280ba6644bd
TomaszPlonski <tom.plonski@gmail.com>
2022-04-25 20:15:42 +0200

    AppInitializer

---
+++ src/main/java/pl/coderslab/AppInitializer.java
@@ -0,0 +1,16 @@
+package pl.coderslab;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() { return null; }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{AppConfig.class};  }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};   }
+}
